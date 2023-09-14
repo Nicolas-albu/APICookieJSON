fc710ebfdacddc2439b548168df5e4622d3acde3
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-09-14 13:11:15 -0300

    password_cryptography function added and key_cryptography updated to use bcrypt

--- APICookieJson/core/security.py
+++ APICookieJson/core/security.py
@@ -1,2 +1,23 @@
+import secrets
+import string
+from pathlib import Path
+
+import bcrypt
+import toml
+
+from .. import SIZE_TOKEN
+
+
+def password_cryptography(_password: str) -> str:
+    with open(Path.cwd() / '.secrets.toml', 'r', encoding='utf-8') as file:
+        _pass = toml.load(file).get('SECRET_KEY', '')
+
+    return str(bcrypt.hashpw(_password.encode('utf-8'), _pass))
+
+
 def key_cryptography(key: str) -> str:
-    ...
+    chars = string.ascii_letters + string.digits + string.punctuation
+
+    return ''.join(secrets.choice(chars) for _ in range(SIZE_TOKEN)) + str(
+        bcrypt.hashpw(key.encode('utf-8'), bcrypt.gensalt())
+    )
