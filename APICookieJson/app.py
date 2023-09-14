d1cdfce2940f0955506d52b5c90b66405e9cb239
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-09-14 13:12:20 -0300

    Secret key of Flask added

--- APICookieJson/app.py
+++ APICookieJson/app.py
@@ -1,7 +1,15 @@
+from pathlib import Path
+
+import toml
 from flask import Flask
 
 from .routes import routes
 
 app = Flask(__name__)
+
+with open(Path.cwd() / '.secrets.toml', 'r', encoding='utf-8') as file:
+    _secret_key = toml.load(file).get('FLASK_SECRET_KEY')
+
+app.secret_key = _secret_key
 
 app.register_blueprint(routes)
