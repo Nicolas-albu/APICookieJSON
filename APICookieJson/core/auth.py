7ce04131e1d8343d23979ed2b715ff7d862f4529
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-09-14 13:08:57 -0300

    _Authentication class added

--- APICookieJson/core/auth.py
+++ APICookieJson/core/auth.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, TypedDict
+from typing import List, TypedDict
 
 
 class _User(TypedDict):
@@ -11,18 +11,29 @@ class _Token(TypedDict):
     user: _User
 
 
-class Authentication:
+class _Authentication:
     def __init__(self):
-        self.__users: _User = {}  # type: ignore
+        self.__users: List[_User] = []
         self.__sessions: List[_Token] = []
 
-    def is_token_in_session(
-        self,
-        token: str,
-        session: Optional[_User] = None,
-    ) -> bool | _User:
+    def is_token_active(self, token: str) -> bool | _User:
         for _session in self.__sessions:
             if token == _session.get("token"):
                 return _session.get("user")
 
         return False
+
+    def is_user(self, username: str, password: str) -> bool:
+        for _user in self.__users:
+            if (username, password) == _user.values():
+                return True
+
+        return False
+
+    def set_session(self, token: str, username: str, password: str) -> None:
+        self.__sessions.append(
+            {
+                "token": token,
+                "user": {"name": username, "password": password},
+            }
+        )
